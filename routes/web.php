b7d7f445ad651dbfa40163270f1592cfc12d597f
Mark Wales <mark@developme.tech>
2020-09-15 10:17:55 +0100

    updates routes to work with Laravel 8.0

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
-Route::get('/', "Articles@index");
-Route::get('/search', "Articles@search");
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Articles;
+
+Route::get('/', [Articles::class, "index"]);
+Route::get('/search', [Articles::class, "search"]);
 
 Route::group(["prefix" => "articles"], function () {
     // put behind the auth route
     // need to be logged in to use
     // /create needs to come before /{article}
     Route::group(["middleware" => "auth"], function () {
-        Route::get('create', "Articles@create");
-        Route::post('create', "Articles@createPost");
-        Route::get('{article}/edit', "Articles@edit");
-        Route::post('{article}/edit', "Articles@editPost");
+        Route::get('create', [Articles::class, "create"]);
+        Route::post('create', [Articles::class, "createPost"]);
+        Route::get('{article}/edit', [Articles::class, "edit"]);
+        Route::post('{article}/edit', [Articles::class, "editPost"]);
     });
 
     // /{article} needs to come after /create
-    Route::get('{article}', "Articles@show");
-    Route::post('{article}', "Articles@commentPost");
+    Route::get('{article}', [Articles::class, "show"]);
+    Route::post('{article}', [Articles::class, "commentPost"]);
 });
 
 Auth::routes(['register' => false]);
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
