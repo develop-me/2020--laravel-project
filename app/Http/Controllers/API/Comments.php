4ee0b0119dbaa2ac8d1ee4ca426119afe23d62a2
Mark Wales <mark@developme.tech>
2020-10-21 09:03:18 +0100

    tidies up Comments API Controller

--- app/Http/Controllers/API/Comments.php
+++ app/Http/Controllers/API/Comments.php
@@ -10,28 +10,47 @@ use App\Models\Comment;
 
 class Comments extends Controller
 {
-    public function index()
+    public function index(Article $article)
     {
-        return CommentResource::collection(Comment::all());
+        // return all comments for a specific article
+        return CommentResource::collection($article->comments);
     }
 
     public function store(CommentRequest $request, Article $article)
     {
         $data = $request->all();
-        $comment = new Comment();
-        $comment->fill($data)->article()->associate($article)->save();
+
+        // create a new comment with the data
+        $comment = new Comment($data);
+
+        // associate the comment with an article
+        $comment->article()->associate($article);
+
+        // save the comment in the DB
+        $comment->save();
+
+        // return the new $comment
         return new CommentResource($comment);
     }
 
     public function show(Article $article, Comment $comment)
     {
+        // return the comment
+        // don't actually use $article, but required for route model binding
         return new CommentResource($comment);
     }
 
     public function update(CommentRequest $request, Article $article, Comment $comment)
     {
         $data = $request->all();
-        $comment->fill($data)->article()->associate($article)->save();
+
+        // update the model with new data
+        $comment->fill($data);
+
+        // don't need to associate with article as shouldn't have changed
+        // but $article required for route model binding
+        // save the comment
+        $comment->save();
         return new CommentResource($comment);
     }
 
